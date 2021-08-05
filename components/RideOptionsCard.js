24d59d908bc0698c3a8d0d9c81a6a72349c60710
Anthony-333 <anthonyoling333@gmail.com>
2021-08-05 21:09:01 +0800

    First commit

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RideOptionsCard = () => {
+    return (
+        <View>
+            <Text>Pick ride</Text>
+        </View>
+    )
+}
+
+export default RideOptionsCard
+
+const styles = StyleSheet.create({})
